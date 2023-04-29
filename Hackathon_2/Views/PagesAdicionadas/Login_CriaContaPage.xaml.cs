673d4a42b8ccd5873206336b77b6847b71e6698e
GAMASTI\luiz.lima <luiz.lima@moovefy.com.br>
2023-04-29 18:14:28 -0300

    Criação de tela de login, tla de criação de conta, adicionado titulo, mudança de cor do app

---
+++ Hackathon_2/Views/PagesAdicionadas/Login_CriaContaPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hackathon_2.Views.PagesAdicionadas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login_CriaContaPage : ContentPage
+    {
+        public Login_CriaContaPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new CriaContaPage());
+        }
+    }
+}
